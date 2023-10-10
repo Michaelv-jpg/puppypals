0ba4d5f2097429a70ada8a22af1b4251f47ccb8b
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2023-10-10 15:57:42 -0400

    finished puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList)
+
   return (
     <>
-      <div>
+      <div className='App'>
+  
+        { 
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+       }
        
       </div>
       <h1></h1>
