6133eb73572ee6b7ec308620d548f1fe0c49768a
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2023-10-10 15:24:16 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       
+      </div>
+      <h1></h1>
+      <div className="card">
+        
+      </div>
+      <p className="read-the-docs">
+       
+      </p>
+    </>
+  )
+}
+
+export default App
